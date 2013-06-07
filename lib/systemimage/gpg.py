8188c3dfccbef835c3e88a2a26994dc8576bc1f8
Stéphane Graber <stgraber@ubuntu.com>
2013-06-07 19:12:12 -0400

    Add some keyring management functions.

--- lib/systemimage/gpg.py
+++ lib/systemimage/gpg.py
@@ -15,8 +15,12 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import json
 import gpgme
 import os
+import tarfile
+
+from io import BytesIO
 
 
 def sign_file(key, path, destination=None, detach=True, armor=True):
@@ -62,3 +66,139 @@ def sign_file(key, path, destination=None, detach=True, armor=True):
             retval = ctx.sign(fd_in, fd_out, gpgme.SIG_MODE_NORMAL)
 
     return retval
+
+
+class Keyring:
+    """
+        Represents a keyring, let's you list/add/remove keys and change
+        some of the keyring properties (type, expiration, target hardware)
+    """
+
+    keyring_name = None
+    keyring_type = None
+    keyring_expiry = None
+    keyring_model = None
+    keyring_path = None
+
+    def __init__(self, keyring_name):
+        keyring_path = "gpg/keyrings/%s" % keyring_name
+        if not os.path.isdir(keyring_path):
+            os.makedirs(keyring_path)
+
+        self.keyring_name = keyring_name
+        self.keyring_path = keyring_path
+
+        if os.path.exists("%s/keyring.json" % keyring_path):
+            with open("%s/keyring.json" % keyring_path, "r") as fd:
+                keyring_json = json.loads(fd.read())
+
+            self.keyring_type = keyring_json.get('type', None)
+            self.keyring_expiry = keyring_json.get('expiry', None)
+            self.keyring_model = keyring_json.get('model', None)
+        else:
+            open("%s/pubring.gpg" % keyring_path, "w+").close()
+
+    def generate_tarball(self, destination=None):
+        """
+            Generate a tarball of the keyring and its json metadata.
+        """
+
+        if not destination:
+            destination = "gpg/keyrings/%s.tar" % self.keyring_name
+
+        if os.path.isfile(destination):
+            os.remove(destination)
+
+        tarball = tarfile.open(destination, "w:")
+        tarball.add("%s/keyring.json" % self.keyring_path,
+                    arcname="keyring.json")
+        tarball.add("%s/pubring.gpg" % self.keyring_path,
+                    arcname="keyring.gpg")
+        tarball.close()
+
+    def set_metadata(self, keyring_type, keyring_expiry=None,
+                     keyring_model=None):
+        """
+            Generate a new keyring.json file.
+        """
+
+        keyring_json = {}
+        if keyring_type:
+            self.keyring_type = keyring_type
+            keyring_json['type'] = keyring_type
+
+        if keyring_expiry:
+            self.keyring_expiry = keyring_expiry
+            keyring_json['expiry'] = keyring_expiry
+
+        if keyring_model:
+            self.keyring_model = keyring_model
+            keyring_json['model'] = keyring_model
+
+        with open("%s/keyring.json" % self.keyring_path, "w+") as fd:
+            fd.write(json.dumps(keyring_json))
+
+    def list_keys(self):
+        os.environ['GNUPGHOME'] = self.keyring_path
+
+        keys = []
+
+        ctx = gpgme.Context()
+        for key in ctx.keylist():
+            keys.append((key.subkeys[0].keyid, key.subkeys[0].length,
+                        [uid.uid for uid in key.uids]))
+
+        return keys
+
+    def export_key(self, path, key, armor=True):
+        os.environ['GNUPGHOME'] = self.keyring_path
+
+        ctx = gpgme.Context()
+        ctx.armor = armor
+
+        gpg_key = ctx.get_key(key)
+
+        with open(path, "w+") as fd:
+            for subkey in gpg_key.subkeys:
+                ctx.export(subkey.keyid, fd)
+
+    def import_key(self, path, armor=True):
+        os.environ['GNUPGHOME'] = self.keyring_path
+
+        ctx = gpgme.Context()
+        ctx.armor = armor
+
+        with open(path, "rb") as fd:
+            ctx.import_(fd)
+
+    def import_keys(self, path):
+        """
+            Import all the keys from the specified keyring.
+        """
+
+        os.environ['GNUPGHOME'] = path
+
+        ctx = gpgme.Context()
+
+        keys = []
+        for key in list(ctx.keylist()):
+            for subkey in key.subkeys:
+                content = BytesIO()
+                ctx.export(subkey.keyid, content)
+                keys.append(content)
+
+        os.environ['GNUPGHOME'] = self.keyring_path
+        ctx = gpgme.Context()
+
+        for key in keys:
+            key.seek(0)
+            ctx.import_(key)
+
+    def del_key(self, key):
+        os.environ['GNUPGHOME'] = self.keyring_path
+
+        ctx = gpgme.Context()
+
+        gpg_key = ctx.get_key(key)
+
+        ctx.delete(gpg_key)
--- tests/test_gpg.py
+++ tests/test_gpg.py
@@ -1,10 +1,11 @@
 import glob
 import os
 import shutil
-import tarfile
 import tempfile
+import time
 import unittest
 
+from gpgme import GpgmeError
 from systemimage import gpg
 
 
@@ -45,7 +46,7 @@ class DiffTests(unittest.TestCase):
         # Standard binary signature
         [os.remove(path) for path in glob.glob("%s.*" % test_file)]
         self.assertTrue(gpg.sign_file("signing", test_file, detach=False,
-                                        armor=False))
+                                      armor=False))
         self.assertTrue(os.path.exists("%s.gpg" % test_file))
 
         # Failure cases
@@ -54,3 +55,53 @@ class DiffTests(unittest.TestCase):
         gpg.sign_file("signing", test_file)
         self.assertRaises(Exception, gpg.sign_file, "signing", test_file)
         self.assertRaises(Exception, gpg.sign_file, "signing", "invalid")
+
+    def test_keyring(self):
+        keyring = gpg.Keyring("testing")
+        self.assertTrue(os.path.exists("gpg/keyrings/testing"))
+        self.assertEquals(keyring.keyring_model, None)
+        self.assertEquals(keyring.keyring_name, "testing")
+        self.assertEquals(keyring.keyring_type, None)
+        self.assertEquals(keyring.keyring_expiry, None)
+
+        expiry = int(time.strftime("%s", time.gmtime()))
+        keyring.set_metadata(keyring_type="test", keyring_model="test",
+                             keyring_expiry=expiry)
+
+        keyring = gpg.Keyring("testing")
+        self.assertEquals(keyring.keyring_model, "test")
+        self.assertEquals(keyring.keyring_name, "testing")
+        self.assertEquals(keyring.keyring_type, "test")
+        self.assertEquals(keyring.keyring_expiry, expiry)
+
+        keyring.import_keys("gpg/keys/signing/")
+        self.assertEquals(
+            keyring.list_keys(),
+            [('29CAF00B0F6342D3', 2048,
+              ['[FAKE] Ubuntu System Image Signing Key (2013) '
+               '<system-image@ubuntu.com>'])])
+
+        temp_key = "%s/key.asc" % self.temp_directory
+        keyring.export_key(temp_key, "29CAF00B0F6342D3")
+        self.assertTrue(os.path.exists(temp_key))
+        keyring.del_key("29CAF00B0F6342D3")
+        self.assertEquals(keyring.list_keys(), [])
+        keyring.import_key(temp_key)
+        self.assertEquals(
+            keyring.list_keys(),
+            [('29CAF00B0F6342D3', 2048,
+              ['[FAKE] Ubuntu System Image Signing Key (2013) '
+               '<system-image@ubuntu.com>'])])
+        self.assertRaises(GpgmeError, keyring.export_key, "missing", "abcd")
+        self.assertRaises(GpgmeError, keyring.del_key, "abcd")
+
+        temp_tarball = "%s/keyring.tar" % self.temp_directory
+        keyring.generate_tarball(temp_tarball)
+        keyring.generate_tarball(temp_tarball)
+        self.assertTrue(os.path.exists(temp_tarball))
+
+        keyring.generate_tarball()
+        self.assertTrue(os.path.exists("gpg/keyrings/testing.tar"))
+
+        os.remove("gpg/keyrings/testing.tar")
+        shutil.rmtree("gpg/keyrings/testing")
