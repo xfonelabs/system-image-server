91e8e04aaee0109b7c7e27a09eb688017958ad81
Stéphane Graber <stgraber@ubuntu.com>
2013-06-10 14:53:33 -0400

    Add missing license headers, rename test classes to make more sense, update GPG testsuite to require test keys.

--- tests/test_gpg.py
+++ tests/test_gpg.py
@@ -1,15 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Stéphane Graber <stgraber@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import glob
+import gpgme
 import os
 import shutil
 import tempfile
 import time
 import unittest
 
-from gpgme import GpgmeError
 from systemimage import gpg
 
 
-class DiffTests(unittest.TestCase):
+class GPGTests(unittest.TestCase):
     def setUp(self):
         temp_directory = tempfile.mkdtemp()
         self.temp_directory = temp_directory
@@ -17,6 +34,9 @@ class DiffTests(unittest.TestCase):
     def tearDown(self):
         shutil.rmtree(self.temp_directory)
 
+
+    @unittest.skipIf(not os.path.exists("tests/keys/generated"),
+                     "No GPG testing keys present. Run tests/generate-keys")
     def test_sign_file(self):
         if not os.path.isdir("gpg/keys/signing"):
             print("Missing signing key, can't proceed")
@@ -56,6 +76,8 @@ class DiffTests(unittest.TestCase):
         self.assertRaises(Exception, gpg.sign_file, "signing", test_file)
         self.assertRaises(Exception, gpg.sign_file, "signing", "invalid")
 
+    @unittest.skipIf(not os.path.exists("tests/keys/generated"),
+                     "No GPG testing keys present. Run tests/generate-keys")
     def test_keyring(self):
         keyring = gpg.Keyring("testing")
         self.assertTrue(os.path.exists("gpg/keyrings/testing"))
@@ -92,8 +114,9 @@ class DiffTests(unittest.TestCase):
             [('29CAF00B0F6342D3', 2048,
               ['[FAKE] Ubuntu System Image Signing Key (2013) '
                '<system-image@ubuntu.com>'])])
-        self.assertRaises(GpgmeError, keyring.export_key, "missing", "abcd")
-        self.assertRaises(GpgmeError, keyring.del_key, "abcd")
+        self.assertRaises(gpgme.GpgmeError, keyring.export_key,
+                          "missing", "abcd")
+        self.assertRaises(gpgme.GpgmeError, keyring.del_key, "abcd")
 
         temp_tarball = "%s/keyring.tar" % self.temp_directory
         keyring.generate_tarball(temp_tarball)
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2013 Canonical Ltd.
+# Author: Stéphane Graber <stgraber@ubuntu.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import shutil
 import tarfile
@@ -7,7 +24,7 @@ import unittest
 from systemimage import tools
 
 
-class DiffTests(unittest.TestCase):
+class ToolTests(unittest.TestCase):
     def setUp(self):
         temp_directory = tempfile.mkdtemp()
         self.temp_directory = temp_directory
