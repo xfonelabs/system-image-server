3339e17392f2f3c20af6dad1061a4ce978a7eb6f
Stéphane Graber <stgraber@ubuntu.com>
2014-03-05 16:09:17 -0500

    Only rename the channel path if it exists

--- lib/systemimage/tree.py
+++ lib/systemimage/tree.py
@@ -536,7 +536,8 @@ class Tree:
 
                 if not os.path.exists(os.path.dirname(new_channel_path)):
                     os.makedirs(os.path.dirname(new_channel_path))
-                os.rename(old_channel_path, new_channel_path)
+                if os.path.exists(old_channel_path):
+                    os.rename(old_channel_path, new_channel_path)
 
             channels[new_name] = dict(channels[old_name])
             if "redirect" not in channels[new_name]:
