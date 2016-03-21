fb69682b9984b0bbc6f1a8b1d9675ac0c8da1f5a
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2016-03-21 14:22:19 +0100

    Temporarily work-around the fact that there are multiple keys in the image-signing key to unblock imports. Waiting for someone to clear out the situation.

--- lib/systemimage/gpg.py
+++ lib/systemimage/gpg.py
@@ -89,7 +89,8 @@ def sign_file(config, key, path, destination=None, detach=True, armor=True):
     # Create a GPG context, assuming no passphrase
     ctx = gpgme.Context()
     ctx.armor = armor
-    [key] = ctx.keylist()
+    [key] = ctx.keylist()[0] # XXX: This is a temporary workaround until the
+                             #  key situation is explained
     ctx.signers = [key]
 
     logger.debug("Signing file: %s" % destination)
